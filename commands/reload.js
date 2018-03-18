1e14d5d83df98c1dec06f0e23c441bd12d1a6f10
Nostalgia Ninja <Nostalgia.ninja@gmail.com>
2018-03-18 04:19:01 +0200

    fixing a small error which gets caught by the control panel

--- commands/reload.js
+++ commands/reload.js
@@ -8,7 +8,7 @@ exports.run = (client,message,args) =>
             if (!args || args.size < 1) return message.channel.send("please specify a command name to reload!");
             
             delete require.cache[require.resolve(`./${args[0]}.js`)];  //deletes the cache for the bot, since node.js is cache-based.
-            message.channel.send(`the command ${args[0]} has been reloaded`).delete(10000);
+            message.channel.send(`the command ${args[0]} has been reloaded`);
         }
         catch (e)
         {
